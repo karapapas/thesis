5c45d0d48cf5d7c88d048d4dcce7f36f5b2245b7
christos <karapapas@outlook.com>
2020-10-14 20:59:26 +0300

    improve generateMetrics function to print metrics for multiple models and also to print the relevant roc curves in one plot

--- moduleMetrics.py
+++ moduleMetrics.py
@@ -1,22 +1,23 @@
-from sklearn.model_selection import cross_val_score
 import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import plot_roc_curve, plot_confusion_matrix
 
 class MetricsMethods:
 
-    # plot_roc_curve(model_LR, X_test, y_test)
-    # # disp = plot_roc_curve(model_LR, X_test, y_test)
-    # # plot_roc_curve(model_DTC, X_test, y_test, ax=disp.ax_)
-    # # plot_roc_curve(model_RFC, X_test, y_test, ax=disp.ax_)
-
     # Based on the example at https://scikit-learn.org/stable/modules/model_evaluation.html
-    def generateMetrics(self, model, X, y, metrics):
-
-        for metric in metrics:
-            results = cross_val_score(model, X, y, cv=5, scoring=metric)
-            raw = np.array2string(results, threshold=np.inf, max_line_width=np.inf, separator=',').replace('\n', '')
-            print("Metric: ", metric, " Score: %.3f%% Std.: %.3f%% Raw Results: %s" % (results.mean() * 100.0,
-                                                                                       results.std() * 100.0,
-                                                                                       raw))
+    def generateMetrics(self, models, X, y, metrics):
+        axes = None
+        for idx, model in enumerate(models):
+            for metric in metrics:
+                results = cross_val_score(model, X, y, cv=5, scoring=metric)
+                raw = np.array2string(results, threshold=np.inf, max_line_width=np.inf, separator=',').replace('\n', '')
+                # print("Metric: ", metric, " Score: %.3f%% Std.: %.3f%% Raw Results: %s" % (results.mean() * 100.0, results.std() * 100.0, raw))
+                print("Metric: ", metric, " Score: %.3f%% Std.: %.3f%%" % (results.mean() * 100.0, results.std() * 100.0))
+            if idx == 0:
+                display = plot_roc_curve(model, X, y)
+                axes = display.ax_
+            else:
+                plot_roc_curve(model, X, y, ax=axes)
 
     # TODO
     #  include prediction probabilities
