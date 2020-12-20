f6d3aa40b837d8803b7f054abf2e28406de56bff
christos <karapapas@outlook.com>
2020-12-20 16:15:44 +0200

    create custom specificity. add option to show probabilities on decision surface.

--- moduleMetrics.py
+++ moduleMetrics.py
@@ -9,45 +9,32 @@ from numpy import meshgrid
 from numpy import arange
 from numpy import hstack
 
-# if metric == 'scorer_specificity':
-#     metric = make_scorer(custom_specificity, greater_is_better=False)
-#
-# def custom_measure(y_actual, y_hat):
-#     tp, fp, tn, fn = 0, 0, 0, 0
-#     for i in range(len(y_hat)):
-#         if y_actual[i] == y_hat[i] == 1:
-#             tp += 1
-#         if y_hat[i] == 1 and y_actual[i] == 0:
-#             fp += 1
-#         if y_hat[i] == y_actual[i] == 0:
-#             tn += 1
-#         if y_hat[i] == 0 and y_actual[i] == 1:
-#             fn += 1
-#     return tp, fp, tn, fn
-#
-#
-# def custom_specificity(y_true, y_pred):
-#     print('y_true', y_true, 'y_pred', y_pred)
-#     tp, fp, tn, fn = custom_measure(y_true, y_pred)
-#     test = tn / (tn + fp)
-#     print('test', test)
-#     print('test type', type(test))
-#     return test
+
+def custom_measure(y_actual, y_hat):
+    cm = confusion_matrix(y_actual, y_hat)
+    tn = cm[0][0]
+    fn = cm[1][0]
+    tp = cm[1][1]
+    fp = cm[0][1]
+    return tp, fp, tn, fn
+
+
+def custom_specificity(y_true, y_pred):
+    tp, fp, tn, fn = custom_measure(y_true, y_pred)
+    if tn + fp == 0:
+        return 1
+    else:
+        return tn / (tn + fp)
 
 
 class MetricsMethods:
-
-    # @staticmethod
-    # def custom_precision(self, y, y_pred):
-    #     tp, fp, tn, fn = custom_measure(y, y_pred)
-    #     return tp / (tp + fp)
 
     # Based on the example at https://scikit-learn.org/stable/modules/model_evaluation.html
     @staticmethod
     def generate_metrics(models, x_test, y_test, metrics, cv_num, show_raw_data):
 
-        print("Count of label NC in y_test: {}".format(sum(y_test == 2)))
-        print("Count of label AD-MCI in y_test: {} \n".format(sum(y_test == 1)))
+        print("Count of label NC (id 2 in Database, 1 in CM) in y_test: {}".format(sum(y_test == 2)))
+        print("Count of label AD-MCI (id 1 in Database, 0 in CM) in y_test: {} \n".format(sum(y_test == 1)))
 
         # for roc curves
         roc_axes = None
@@ -63,30 +50,35 @@ class MetricsMethods:
         y_test_m = y_test
         y_test_cm = y_test
         for idx, (model_name, model) in enumerate(models.items()):
+            if show_raw_data:
+                print("\n", model_name + " Scores")
 
             # print given metrics in boxplot
             # boxplot example
             # https://machinelearningmastery.com/compare-machine-learning-algorithms-python-scikit-learn/
             mbox = pd.DataFrame()
             for metric in metrics:
+                metric_name = metric
+                if metric == 'specificity':
+                    metric = make_scorer(custom_specificity)
                 results = cross_val_score(model, x_test_m, y_test_m, cv=cv_num, scoring=metric)
+                # print('results', type(results))
                 if show_raw_data:
                     raw = np.array2string(results, threshold=np.inf, max_line_width=np.inf, separator=',')
                     raw.replace('\n', '').replace(' ', '')
-                    print(metric, " Score: %.3f%% Std.: %.3f%%" % (results.mean() * 100.0, results.std() * 100.0))
-                    print("Metric raw data: ", raw)
-                mbox[metric] = results
+                    print(metric, ": %.3f%% Std.: %.3f%%" % (results.mean() * 100.0, results.std() * 100.0))
+                    # print("Metric raw data: ", raw)
+                if isinstance(type(metric), str):
+                    mbox[metric] = results
+                else:
+                    mbox[metric_name] = results
 
             # plot confusion matrix
             # based on the example at https://stackoverflow.com/questions/61825227/
             y_pred = model.predict(x_test_cm)
             cf_matrix[model_name] = confusion_matrix(y_test_cm, y_pred)
 
-            # specificity
-            tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-            mbox['Specificity'] = (tn / (tn + fp))
-
-            fig, (axm, axc) = plt.subplots(1, 2, figsize=(12, 2))
+            fig, (axm, axc) = plt.subplots(1, 2, figsize=(18, 6))
             boxplot_title = 'Metrics for ' + model_name
             sns.boxplot(data=mbox, orient="h", palette="Set3", showmeans=True, ax=axm).set_title(boxplot_title)
             sns.heatmap(cf_matrix[model_name], ax=axc, annot=True, fmt='d', cmap="RdBu")
@@ -111,7 +103,7 @@ class MetricsMethods:
         pr_axes.plot([0, 1], [0.5, 0.5], linestyle='--', lw=2, color='r', label='Chance', alpha=.8)
 
     @staticmethod
-    def print_decision_surface(models, data_set_tuples):
+    def print_decision_surface(models, data_set_tuples, show_probabilities):
 
         for idx, (model_name, model) in enumerate(models.items()):
             fig, axs = plt.subplots(1, 3, figsize=(18, 4))
@@ -142,10 +134,13 @@ class MetricsMethods:
 
                 # horizontal stack vectors to create x1,x2 input for the model
                 grid = hstack((r1, r2))
-                y_hat = model.predict_proba(grid)
 
-                # keep just the probabilities for class 0
-                y_hat = y_hat[:, 0]
+                if show_probabilities is True:
+                    y_hat = model.predict_proba(grid)
+                    # keep just the probabilities for class 0
+                    y_hat = y_hat[:, 0]
+                else:
+                    y_hat = model.predict(grid)
 
                 # reshape the predictions back into a grid
                 zz = y_hat.reshape(xx.shape)
